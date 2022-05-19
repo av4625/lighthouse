962d51246d3c72d69bf5f6bbf607d5dff5a2e94b
Aaron Vance <av4625@gmail.com>
2022-05-19 20:24:12 +0100

    Add cyon and colour rotation

--- README.md
+++ README.md
@@ -7,20 +7,31 @@ the top. I will use a Digispark microcontroller to keep the size small although
 the available memory is very limited.
 
 ## To-Do
-- [ ] Fade one is and then one out and on to the next (and for two at a time)
+- [ ] Fade one in and then one out and on to the next (and for two at a time)
     - [ ] White only
     - [ ] Multi coloured
 
-- [ ] Cyon effect (and for two at a time)
-   - [ ] White only
-   - [ ] Multi coloured
+- [x] Cyon effect (and for two at a time)
+   - [x] White only
+   - [x] Multi coloured (Change colour for each light)
+   - [ ] Multi coloured (Change colour for each loop)
+   - [ ] Don't fade to black, have all LED's fade to "dim"
 
 - [ ] Random light sharp on and fade out (and for two at a time)
     - [ ] White only
     - [ ] Multi coloured (Random colour and rainbow pattern)
 
-- [ ] Always on but colour rotates (and for two at a time)
+- [x] Always on but colour rotates (and for two at a time)
+    - [ ] Fade in and out at the same time
 
 - [ ] All on but the faded one rotates (like a reverse cyon (and for two at a time))
     - [ ] White only
     - [ ] Multi coloured
+
+- [ ] Fade in and out
+
+- [ ] Multiple LED's circling
+    - [ ] White only
+    - [ ] Multi coloured?
+
+- [ ] Make some change direction?
--- lighthouse.ino
+++ lighthouse.ino
@@ -1,6 +1,7 @@
 #include <FastLED.h>
+
 const uint8_t number_of_leds(16);
-CRGB leds[number_of_leds];
+CRGBArray<number_of_leds> leds;
 
 void setup()
 {
@@ -11,23 +12,133 @@ void setup()
             leds, number_of_leds, 255, 5, 475));
 }
 
+bool has_delay_elapsed(const short delay)
+{
+    static unsigned long previous_time(0);
+    const unsigned long current_time(millis());
+
+    if (current_time > (previous_time + delay))
+    {
+        previous_time = current_time;
+        return true;
+    }
+
+    return false;
+}
+
+void cyon(
+    const short delay,
+    const uint8_t brightness,
+    const bool is_white_only,
+    const bool is_one_at_a_time)
+{
+    static uint8_t index(1);
+    static uint8_t colour(0);
+
+    if (has_delay_elapsed(delay))
+    {
+        if (is_one_at_a_time)
+        {
+            leds[index] = CHSV(
+                colour,
+                (is_white_only) ? 0 : 255,
+                brightness);
+
+            index = (index + 1) % number_of_leds;
+        }
+        else
+        {
+            // To get the correct groups of two for my dividers
+            if (index % 2 != 1)
+            {
+                index = 1;
+            }
+
+            const uint8_t second_index((index + 1) % number_of_leds);
+
+            // This is needed to wrap round to the start
+            if (second_index < index)
+            {
+                leds[index] = CHSV(
+                    colour,
+                    (is_white_only) ? 0 : 255,
+                    brightness);
+                leds[second_index] = CHSV(
+                    colour,
+                    (is_white_only) ? 0 : 255,
+                    brightness);
+            }
+            else
+            {
+                leds(index, (index + 1) % number_of_leds) = CHSV(
+                    colour,
+                    (is_white_only) ? 0 : 255,
+                    brightness);
+            }
+
+            index = (index + 2) % number_of_leds;
+        }
+
+        FastLED.show();
+        // Make this configurable?
+        fade_all(75);
+        // Make this configurable?
+        colour += 8;
+    }
+}
+
+void colour_rotation(
+    const short delay, const uint8_t brightness, const bool is_one_at_a_time)
+{
+    static uint8_t colour(0);
+
+    if (has_delay_elapsed(delay))
+    {
+        if (is_one_at_a_time)
+        {
+            for (CRGB& led : leds)
+            {
+                // 15 is 255 / 17 (1 extra led) to make it rotate
+                led = CHSV(colour += 15, 255, brightness);
+            }
+        }
+        else
+        {
+            for (uint8_t i = 1; i < number_of_leds; i += 2)
+            {
+                // 28.33333 is 255 / 9 (1 extra section) to make it rotate
+                leds[i] = CHSV(colour, 255, brightness);
+
+                /* Starting at 1 and have this if to get the correct groups of
+                 * two for my dividers
+                 */
+                if (i != number_of_leds - 1)
+                {
+                    leds[i + 1] = CHSV(colour, 255, brightness);
+                }
+                else
+                {
+                    leds[0] = CHSV(colour, 255, brightness);
+                }
+
+                colour += 28;
+            }
+        }
+
+        FastLED.show();
+    }
+}
+
+void fade_all(const uint8_t nscale)
+{
+    leds.fadeToBlackBy(nscale);
+}
+
 uint8_t colour(0);
 uint8_t i(0);
 
 void loop()
 {
-    leds[i] = CHSV(colour, 255, 75);
-    FastLED.show();
-    fade_all(200);
-    delay(75);
-    colour += 10;
-    i = ((i + 1) % number_of_leds);
-}
-
-void fade_all(const uint8_t nscale)
-{
-    for(uint8_t i = 0; i < number_of_leds; ++i)
-    {
-        leds[i].nscale8(nscale);
-    }
+    // cyon(250, 80, false, false);
+    colour_rotation(200, 100, false);
 }
