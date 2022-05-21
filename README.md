848baa870fed5acd8ad32920a1a7754fdc3ced2e
Aaron Vance <av4625@gmail.com>
2022-05-21 20:23:47 +0100

    Add animations

--- README.md
+++ README.md
@@ -12,14 +12,20 @@ the available memory is very limited.
     - [ ] Multi coloured
 
 - [x] Cyon effect (and for two at a time)
-   - [x] White only
-   - [x] Multi coloured (Change colour for each light)
-   - [ ] Multi coloured (Change colour for each loop)
-   - [ ] Don't fade to black, have all LED's fade to "dim"
+    - [x] White only
+    - [x] Multi coloured (Change colour for each light)
+    - [ ] Multi coloured (Change colour for each loop)
+    - [ ] Don't fade to black, have all LED's fade to "dim"
 
-- [ ] Random light sharp on and fade out (and for two at a time)
-    - [ ] White only
-    - [ ] Multi coloured (Random colour and rainbow pattern)
+- [x] Two LED's spinning (with and without cyon)
+    - [x] Two LED's the same colour and change colour together
+    - [x] White only
+    - [x] Two LED's different colours but add 16 (255 / num LED's) each time so
+          the colours stay in the same place
+
+- [x] Random light sharp on and fade out (and for two at a time)
+    - [x] White only
+    - [x] Multi coloured (Random colour and rainbow pattern)
 
 - [x] Always on but colour rotates (and for two at a time)
     - [ ] Fade in and out at the same time
@@ -28,10 +34,29 @@ the available memory is very limited.
     - [ ] White only
     - [ ] Multi coloured
 
-- [ ] Fade in and out
-
-- [ ] Multiple LED's circling
-    - [ ] White only
-    - [ ] Multi coloured?
+- [x] Fade in and out (Don't think I ike it)
 
 - [ ] Make some change direction?
+
+## Functions
+Some (all) of the animation functions are probably way more configurable than
+they will ever need to be. The animation functions are designed to be
+continually called.
+
+None of the functions block.
+
+- `setup`: Adds the LED's and sets the max brightness that can be used for the
+number of LED's given a max milliamps.
+
+- `has_delay_elapsed`: Returns `true` if the passed in `delay` has elapsed
+since the last call else `false`.
+    - `delay`: The delay to check in milliseconds.
+
+- `cyon`: A circular pattern that looks like it leaves a tail as the tail
+gradually fades out.
+    - `delay`: The millisecond wait moving to the next LED.
+    - `brightness`: The brightness for the leading LED's. Value between 0 - 255.
+    - `is_white_only`: If true only the colour white will be used else the
+    colour will change through a rainbow of colours.
+    - `is_one_at_a_time`: If true there will be one leading LED else there will
+    a leading group of two LED's.
--- lighthouse.ino
+++ lighthouse.ino
@@ -44,7 +44,7 @@ void cyon(
                 (is_white_only) ? 0 : 255,
                 brightness);
 
-            index = (index + 1) % number_of_leds;
+            ++index %= number_of_leds;
         }
         else
         {
@@ -84,6 +84,67 @@ void cyon(
         fade_all(75);
         // Make this configurable?
         colour += 8;
+    }
+}
+
+void multiple_spinning_leds(
+    const short delay,
+    const uint8_t brightness,
+    const bool is_white_only,
+    const bool is_same_colour,
+    const bool cyon_effect = true)
+{
+    // Adding this so we don't have to divide everytime for a "const"
+    static const uint8_t half_leds(number_of_leds / 2);
+    // Integer division round up
+    static const uint8_t colour_change_per_led(
+        (255 + (number_of_leds - 1)) / number_of_leds);
+    static const uint8_t half_colour(colour_change_per_led * half_leds);
+
+    static uint8_t index(0);
+    static uint8_t colour(0);
+
+    if (has_delay_elapsed(delay))
+    {
+        const uint8_t second_index((index + half_leds) % number_of_leds);
+
+        if (is_same_colour)
+        {
+            leds[index] = CHSV(
+                colour,
+                (is_white_only) ? 0 : 255,
+                brightness);
+            leds[second_index] = CHSV(
+                colour,
+                (is_white_only) ? 0 : 255,
+                brightness);
+        }
+        else
+        {
+            leds[index] = CHSV(
+                colour,
+                (is_white_only) ? 0 : 255,
+                brightness);
+            leds[second_index] = CHSV(
+                colour + half_colour,
+                (is_white_only) ? 0 : 255,
+                brightness);
+        }
+
+        if (cyon_effect)
+        {
+            FastLED.show();
+            // Make this configurable?
+            fade_all(75);
+        }
+        else
+        {
+            set_black_except(index, second_index);
+            FastLED.show();
+        }
+
+        ++index %= number_of_leds;
+        colour += colour_change_per_led;
     }
 }
 
@@ -129,16 +190,144 @@ void colour_rotation(
     }
 }
 
+void fade_in_and_out(
+    const short delay, const uint8_t brightness, const bool is_white_only)
+{
+    static uint8_t colour(0);
+    static uint8_t current_brightness(0);
+    static bool fading_out(false);
+
+    if (has_delay_elapsed(delay))
+    {
+        for (CRGB& led : leds)
+        {
+            // 15 is 255 / 17 (1 extra led) to make it rotate
+            led = CHSV(colour, (is_white_only) ? 0 : 255, current_brightness);
+        }
+
+        FastLED.show();
+
+        if (current_brightness == 0)
+        {
+            colour += 10;
+            fading_out = false;
+        }
+        else if (current_brightness == brightness)
+        {
+            fading_out = true;
+        }
+
+        if (fading_out)
+        {
+            --current_brightness;
+        }
+        else
+        {
+            ++current_brightness;
+        }
+    }
+}
+
+// Must be set to black first
+void led_flash_and_fade(
+    const short delay,
+    const uint8_t brightness,
+    const bool is_white_only,
+    const bool is_one_at_a_time,
+    const bool cycle_colours,
+    const bool random_led)
+{
+    static uint8_t current_index(1);
+    static uint8_t current_colour(0);
+    static uint8_t current_brightness(brightness);
+
+    if (has_delay_elapsed(delay))
+    {
+        leds[current_index] = CHSV(
+            current_colour, (is_white_only) ? 0 : 255, --current_brightness);
+
+        if (!is_one_at_a_time)
+        {
+            uint8_t second_index;
+
+            // Need this to work with my dividers
+            if (current_index == 0)
+            {
+                second_index = number_of_leds - 1;
+            }
+            else if (current_index % 2 == 1)
+            {
+                second_index = (current_index + 1) % number_of_leds;
+            }
+            else
+            {
+                second_index = current_index - 1;
+            }
+
+            leds[second_index] = CHSV(
+                current_colour, (is_white_only) ? 0 : 255, current_brightness);
+        }
+
+        FastLED.show();
+    }
+
+    if (current_brightness == 0)
+    {
+        current_brightness = brightness;
+
+        if (random_led)
+        {
+            current_index = random8(0, number_of_leds - 1);
+        }
+        else
+        {
+            if (is_one_at_a_time)
+            {
+                ++current_index %= number_of_leds;
+            }
+            else
+            {
+                current_index = (current_index + 2) % number_of_leds;
+            }
+        }
+
+        if (cycle_colours)
+        {
+            current_colour += 10;
+        }
+        else
+        {
+            current_colour = random8();
+        }
+    }
+}
+
 void fade_all(const uint8_t nscale)
 {
     leds.fadeToBlackBy(nscale);
 }
 
-uint8_t colour(0);
-uint8_t i(0);
+void set_all_black()
+{
+    leds = CRGB::Black;
+}
+
+void set_black_except(const uint8_t index_1, const uint8_t index_2)
+{
+    for (uint8_t i = 0; i < number_of_leds; ++i)
+    {
+        if (i != index_1 && i != index_2)
+        {
+            leds[i] = CRGB::Black;
+        }
+    }
+}
 
 void loop()
 {
     // cyon(250, 80, false, false);
-    colour_rotation(200, 100, false);
+    // colour_rotation(200, 100, false);
+    // multiple_spinning_leds(200, 100, true, true);
+    // fade_in_and_out(15, 125, false);
+    led_flash_and_fade(5, 125, false, true, true, false);
 }
